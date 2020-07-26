a6ac821cd3e626718baf592a64305fc631e08d55
ryan <rigphoria@gmail.com>
2020-07-26 12:48:24 -0400

    stole tutorial code to make harvesters use extensions

--- role.harvester.js
+++ role.harvester.js
@@ -13,9 +13,20 @@ var roleHarvester = {
         creep.memory.isHarvesting = false;
       }
     } else {
-      if(creep.transfer(Game.spawns["Spawn1"], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE){
-        creep.moveTo(Game.spawns["Spawn1"]);
-      }
+      //var extensions = Game.spawns.Spawn1.room.find(FIND_STRUCTURES, {filter: {structureType: STRUCTURE_EXTENSION}});
+      var targets = creep.room.find(FIND_STRUCTURES, {
+          filter: (structure) => {
+                  return (structure.structureType == STRUCTURE_EXTENSION ||
+                      structure.structureType == STRUCTURE_SPAWN ||
+                      structure.structureType == STRUCTURE_TOWER) &&
+                      structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+              }
+          });
+          if(targets.length > 0) {
+              if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                  creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+              }
+          }
 
       if(creep.store.getFreeCapacity() == creep.store.getCapacity()){
         creep.memory.isHarvesting = true;
